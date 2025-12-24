08cdc236b02940eab3e569413fbf385c79a18497
MutableLabs <0000zeitgeist@naver.com>
2025-12-24 14:01:55 +0900

    init

--- example/lesson_2/main.cpp
+++ example/lesson_2/main.cpp
@@ -1,22 +1,43 @@
-#include <optional>
-
 #include <SFML/Graphics.hpp>
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode({200, 200}), "SFML 수업 2");
+    sf::RenderWindow window(sf::VideoMode({800, 800}), "SFML 수업 2");
+    window.setFramerateLimit(60);
 
-    sf::CircleShape shape(100.f);
+    sf::CircleShape circle(50.0f);
+    circle.setFillColor(sf::Color::Green);
+    circle.setPosition({400.0f, 300.0f});
 
-    shape.setFillColor(sf::Color::Green);
+    int       color_index = 0;
+    sf::Color colors[]    = {sf::Color::Red, sf::Color::Blue, sf::Color::Yellow, sf::Color::Magenta};
+
+    float speed = 5.0f;
 
     while (window.isOpen()) {
         while (const std::optional event = window.pollEvent()) {
-            if (event->is<sf::Event::Closed>())
+            if (event->is<sf::Event::Closed>()) {
                 window.close();
+            }
         }
 
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Space)) {
+            color_index = (color_index + 1) % 4;
+            circle.setFillColor(colors[color_index]);
+        }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right))
+            circle.move({speed, 0.f});
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left))
+            circle.move({-speed, 0.f});
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up))
+            circle.move({0.f, -speed});
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down))
+            circle.move({0.f, speed});
+
         window.clear();
-        window.draw(shape);
+        window.draw(circle);
         window.display();
     }
+
+    return 0;
 }
